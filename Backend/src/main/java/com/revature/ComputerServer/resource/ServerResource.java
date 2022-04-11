7e8319a5e85ffb7711596a8311d2e4eac7dc0521
ChristysGitHub <chrndaye@gmail.com>
2022-04-10 22:21:53 -0700

    added resouces to the project

--- Backend/src/main/java/com/revature/ComputerServer/resource/ServerResource.java
+++ Backend/src/main/java/com/revature/ComputerServer/resource/ServerResource.java
@@ -8,14 +8,14 @@ import com.revature.ComputerServer.Model.Server;
 import com.revature.ComputerServer.Service.Implementation.ServerServiceImpl;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.Valid;
+
 import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -44,8 +44,36 @@ public class ServerResource {
         return ResponseEntity.ok(
                 Response.builder()
                         .timeStamp(now())
-                        .data(Map.of("servers", serverService.list(30)))
-                        .message("Servers retrieved")
+                        .data(Map.of("server", server))
+                        .message(server.getStatus() == SERVER_UP ? "Ping success" : "Ping failed")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("server", serverService.create(server)))
+                        .message("Server created")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
+                        .build()
+
+        );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id") Long id){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("server", serverService.get(id)))
+                        .message("Server retrieved")
                         .status(OK)
                         .statusCode(OK.value())
                         .build()

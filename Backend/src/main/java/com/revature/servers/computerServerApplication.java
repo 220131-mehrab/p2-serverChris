37929900ea7ad6a8e55f8c0db29746d959af189e
ChristysGitHub <chrndaye@gmail.com>
2022-04-12 18:28:20 -0700

    modified backend and frontend

---
+++ Backend/src/main/java/com/revature/servers/computerServerApplication.java
@@ -0,0 +1,48 @@
+package com.revature.servers;
+
+import com.revature.servers.enumeration.Status;
+import com.revature.servers.model.Server;
+import com.revature.servers.repository.ServerRepo;
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class computerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(computerServerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo){
+		return args -> {
+			serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/business3.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.190", "Ubuntu Linux", "16 GB", "Company PC", "http://localhost:8080/server/image/donate.png", Status.SERVER_DOWN));
+			serverRepo.save(new Server(null, "192.168.1.140", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/finance.png", Status.SERVER_DOWN));
+		};
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Accept-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
+
+}
